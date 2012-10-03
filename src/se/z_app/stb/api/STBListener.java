6fd051fe3563da835577bc02633042f6d15f53ad
Rasmus Holm <rasmus@island.liu.se>
2012-10-03 17:24:49 +0200

    Adding thread structure to skeleton

--- src/se/z_app/stb/api/STBListener.java
+++ src/se/z_app/stb/api/STBListener.java
@@ -3,7 +3,7 @@ package se.z_app.stb.api;
 import java.util.Observable;
 import java.util.Observer;
 
-public class STBListener extends Observable implements Observer{
+public class STBListener extends Observable implements Observer, Runnable{
 
 	//Singleton and adding itself as an observer
 	private static STBListener instance; 
@@ -20,6 +20,11 @@ public class STBListener extends Observable implements Observer{
 	
 	@Override
 	public void update(Observable observable, Object data) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void run() {
 		// TODO Auto-generated method stub
 		
 	}
