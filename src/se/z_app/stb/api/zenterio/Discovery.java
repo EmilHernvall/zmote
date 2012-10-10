31df5a32d6b46712b3b4e142c38b449aefe4646a
tehvicke <vikda574@student.liu.se>
2012-10-10 15:03:02 +0200

    Added findSubnet() function in Discovery. Started writing the find() function for finding STB's. It should scan the subnet.

--- src/se/z_app/stb/api/zenterio/Discovery.java
+++ src/se/z_app/stb/api/zenterio/Discovery.java
@@ -1,13 +1,48 @@
 package se.z_app.stb.api.zenterio;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import se.z_app.stb.STB;
 import se.z_app.stb.api.DiscoveryInterface;
 
 public class Discovery implements DiscoveryInterface {
 
+	String findSubnet() {
+	    InetAddress addr = null;
+		try {
+			addr = InetAddress.getLocalHost();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    byte[] ipAddr = addr.getAddress();
+	    StringBuffer str = new StringBuffer();
+	    String returnString;
+	    for (int i=0;i < ipAddr.length - 1; i++) {
+	    	int result = ipAddr[i];
+	    	if (ipAddr[i] < 0) {
+	    		result += 256;
+	    	}
+	    	str.append(Integer.toString(result)+".");
+	    }
+	    returnString = str.toString();
+		return returnString;
+	}
+	
 	@Override
 	public STB[] find() {
 		// TODO Auto-generated method stub
+//		try {
+			for (int i = 0; i < 256; i++) {
+				StringBuffer urlString = new StringBuffer();
+				urlString.append("http://"+findSubnet());
+				urlString.append(Integer.toString(i));
+				urlString.append("/cgi-bin/zids_discovery");
+			}
+//		catch (RuntimeException) {
+//			continue;
+//		}
 		return null;
 	}
 
