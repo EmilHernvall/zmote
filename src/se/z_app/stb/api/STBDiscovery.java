8c38eca19fe7960d67dafc695026b6cb991ec921
tehvicke <vikda574@student.liu.se>
2012-10-11 15:52:56 +0200

    Added functions for finding the first STB (which then reveals the mac addresses of other STB's in the network), as well as a function for finding out the subnet (the 192.168.0.) of the device.

--- src/se/z_app/stb/api/STBDiscovery.java
+++ src/se/z_app/stb/api/STBDiscovery.java
@@ -1,5 +1,32 @@
 package se.z_app.stb.api;
 
-public class STBDiscovery {
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
+import se.z_app.stb.STB;
+import se.z_app.stb.api.zenterio.Discovery;
+
+public class STBDiscovery {
+	Discovery disc = new Discovery();
+	
+	
+	public STB[] find() {  
+		STB[] stb;
+		stb = disc.find();
+		
+		return null; 
+		}
+	
+	public String findSubnet() {
+	    InetAddress addr = null;
+			try {
+				addr = InetAddress.getLocalHost();
+				return addr.getHostAddress().toString().substring(0, addr.getHostAddress().lastIndexOf('.')+1);
+			} catch (UnknownHostException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		return null;
+	}
+	
 }
--- src/se/z_app/stb/api/zenterio/Discovery.java
+++ src/se/z_app/stb/api/zenterio/Discovery.java
@@ -1,45 +1,62 @@
 package se.z_app.stb.api.zenterio;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.InetAddress;
+import java.net.URL;
 import java.net.UnknownHostException;
 
 import se.z_app.stb.STB;
 import se.z_app.stb.api.DiscoveryInterface;
 
+import se.z_app.stb.api.STBDiscovery; // Should this be imported?
+
 public class Discovery implements DiscoveryInterface {
 
-	String findSubnet() {
-	    InetAddress addr = null;
-		try {
-			addr = InetAddress.getLocalHost();
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	STBDiscovery stb = new STBDiscovery();
+	
+	/*
+	 * Scans the network for STB's.
+	 * Return an object InetAddress with the IP address to the STB
+	 * TODO: Write better exception handling
+	 */
+	private InetAddress findFirstSTB() {
+		URL url;
+		int timeoutInMs = 30;
+		InetAddress addr;
+		for (int i = 1; i < 255; i++) {
+			try {
+				addr = InetAddress.getByName(stb.findSubnet() + Integer.toString(i));
+				if(addr.isReachable(timeoutInMs)) {
+					url = new URL("http:/"+addr.toString()+"/cgi-bin/zids_discovery/");
+					try {
+						BufferedReader row = new BufferedReader(new InputStreamReader(url.openStream()));
+				    	if(row.readLine().indexOf("Zenterio") > 0) {
+//				    		System.out.println("Found a box at "+addr.getHostAddress().toString()+"!");
+				    		row.close();
+				    		return addr;
+				    	}
+						row.close();
+					}
+					catch(Exception e) {
+//						e.printStackTrace();
+					}
+				}
+			} catch (Exception e) {
+//				e.printStackTrace();
+			}
+			
 		}
-	    byte[] ipAddr = addr.getAddress();
-	    StringBuffer str = new StringBuffer();
-	    String returnString;
-	    for (int i=0;i < ipAddr.length - 1; i++) {
-	    	int result = ipAddr[i];
-	    	if (ipAddr[i] < 0) {
-	    		result += 256;
-	    	}
-	    	str.append(Integer.toString(result)+".");
-	    }
-	    returnString = str.toString();
-		return returnString;
+		System.out.println("fin");
+		return null;
 	}
 	
 	@Override
 	public STB[] find() {
 		// TODO Auto-generated method stub
 //		try {
-			for (int i = 0; i < 256; i++) {
-				StringBuffer urlString = new StringBuffer();
-				urlString.append("http://"+findSubnet());
-				urlString.append(Integer.toString(i));
-				urlString.append("/cgi-bin/zids_discovery");
-			}
+//			
+//			}
 //		catch (RuntimeException) {
 //			continue;
 //		}
