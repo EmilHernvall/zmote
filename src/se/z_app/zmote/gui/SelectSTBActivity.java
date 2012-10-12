af594b748b14604faa5b9656f35d8803176d399b
tehvicke <vikda574@student.liu.se>
2012-10-12 18:13:09 +0200

    The functions work asynchronously now. I made the Discovery.java asynced.

--- src/se/z_app/zmote/gui/SelectSTBActivity.java
+++ src/se/z_app/zmote/gui/SelectSTBActivity.java
@@ -1,19 +1,9 @@
 package se.z_app.zmote.gui;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.LinkedList;
-
-import se.z_app.stb.STB;
-import se.z_app.stb.api.DiscoveryInterface;
-import se.z_app.stb.api.STBDiscovery;
 import se.z_app.stb.api.zenterio.Discovery;
-import android.net.wifi.WifiInfo;
-import android.net.wifi.WifiManager;
+
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -22,31 +12,23 @@ import android.support.v4.app.NavUtils;
 
 
 /* 
- * TODO: Fix everything. Like findSubnet. It's shit right now // Viktor D
+ * Whoop.
  */
 public class SelectSTBActivity extends Activity {
-	Discovery Disc = new Discovery();
-    STB[] stbs;
-    STBDiscovery stbDisc = new STBDiscovery();
-    LinkedList<InetAddress> boxes = new LinkedList<InetAddress>();
+    private Discovery disc;
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_stb);
-        
-        /*
-         * A button for scanning for STB's
-         */
-        Button next = (Button) findViewById(R.id.button_scanforstb);
-        next.setOnClickListener(new View.OnClickListener() {
+        Button scan = (Button) findViewById(R.id.button_scanforstb);
+        scan.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-            	System.out.println("Button presdsed.");
-            	stbs = Disc.find();
-            	
-            	}
+            		disc = new Discovery();
+            		disc.execute();
+//            	}
+            }
         });
-        
     }
 
     @Override
@@ -54,7 +36,6 @@ public class SelectSTBActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_select_stb, menu);
         return true;
     }
-
     
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -65,5 +46,4 @@ public class SelectSTBActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
-
 }
