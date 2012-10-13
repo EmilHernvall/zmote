776bb7f06d9f51e80b4e4fe9a1618202a19d1925
Rasmus Holm <rasmus@island.liu.se>
2012-10-13 12:36:56 +0200

    Finiched with EPGData and movin on to StandardCommand for implmentation

--- src/se/z_app/stb/api/zenterio/StandardCommand.java
+++ src/se/z_app/stb/api/zenterio/StandardCommand.java
@@ -1,5 +1,15 @@
 package se.z_app.stb.api.zenterio;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import android.graphics.Bitmap;
 import se.z_app.stb.Channel;
 import se.z_app.stb.EPG;
@@ -8,22 +18,43 @@ import se.z_app.stb.WebTVService;
 import se.z_app.stb.api.BiDirectionalCmdInterface;
 
 public class StandardCommand implements BiDirectionalCmdInterface{
+	private String ip;
 
-
+	public StandardCommand(String ip){
+		this.ip = ip.trim();
+	}
+	
 	public EPG getEPG() {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
 	
 	public Channel getChannel() {
-		// TODO Auto-generated method stub
+		
+		// Create a new HttpClient and Post Header
+	    HttpClient httpclient = new DefaultHttpClient();
+	    HttpPost httpget = new HttpPost("http://" + ip + "/mdio/currentchannel");
+
+	    try {
+	        
+	      
+	       // System.out.println("Sending Post: " + postText);
+	        HttpResponse response = httpclient.execute(httpget);
+	        InputStream in = response.getEntity().getContent();
+	        
+	    } catch (ClientProtocolException e) {
+	        // TODO Auto-generated catch block
+	    } catch (IOException e) {
+	        // TODO Auto-generated catch block
+	    }
+		
 		return null;
 	}
 
 	
 	public Bitmap getChannelIcon(Channel channel) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
