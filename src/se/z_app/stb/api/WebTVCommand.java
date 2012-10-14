5b0fc2921dd536c90e55b8c258bd9cd9725ef16d
sebra729 <sebra729@student.liu.se>
2012-10-14 22:28:53 +0200

    Written code skeleton for WebTVCmd

--- src/se/z_app/stb/api/WebTVCommand.java
+++ src/se/z_app/stb/api/WebTVCommand.java
@@ -1,5 +1,67 @@
 package se.z_app.stb.api;
 
-public class WebTVCommand {
+import java.util.Observable;
+import java.util.Observer;
 
+import android.graphics.Bitmap;
+
+import se.z_app.stb.STB;
+import se.z_app.stb.WebTVItem;
+import se.z_app.stb.WebTVService;
+import se.z_app.stb.api.zenterio.RCCommand;
+
+public class WebTVCommand implements Observer{
+
+	private static WebTVCommand instance; 
+	private WebTVCommand(){
+		STBContainer.instance().addObserver(this);
+	}
+	public static WebTVCommand instance(){
+		if(instance == null)
+			instance = new WebTVCommand();
+		return instance;
+	}
+	
+	private MonoDirectionalCmdInterface remoteImpl;
+	@Override
+	
+	
+	public void update(Observable observable, Object data) {
+		STB stb = STBContainer.instance().getSTB();
+		switch(stb.getType()){
+		case DEFAULT:
+			remoteImpl = null;
+			break;
+		case ZENTERIO:
+			remoteImpl = new RCCommand(stb.getIP());
+			break;
+		default:
+			break;
+		}
+	}
+	
+	public WebTVService getSevice(){
+		return null;
+	}
+	
+	public WebTVItem search(){
+		return null;
+	}
+	
+	public void play(){
+		
+	}
+	
+	public void queue(){
+		
+	}
+	
+	public Bitmap getIcon(WebTVItem wti){
+		return null;
+		
+	}
+	
+	public Bitmap getIcon(WebTVService wts){
+		return null;
+	}
 }
