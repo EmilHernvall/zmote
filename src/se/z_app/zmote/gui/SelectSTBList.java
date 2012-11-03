bb383b6b7b8b0b7995f19abfc6c09085468d0a34
ChristianVestman <chrve033@student.liu.se>
2012-11-03 15:24:29 +0100

    Added more comments for java documentation

--- src/se/z_app/zmote/gui/SelectSTBList.java
+++ src/se/z_app/zmote/gui/SelectSTBList.java
@@ -4,24 +4,27 @@ import java.util.Vector;
 
 import se.z_app.stb.STB;
 
-/** Class that holds the list of STBs, will probably not be used
- *   once the databse is implemented.
+/**
+ * Class that holds the list of STBs, will probably not be used once the databse
+ * is implemented.
+ * 
+ * @author Marcus Widegren, Christian Vestman
  */
 public class SelectSTBList {
 	private Vector<STB> theList = new Vector<STB>();
 	private static SelectSTBList theInstance;
-	
-	private SelectSTBList(){}
-	
+
+	private SelectSTBList() {
+	}
+
 	public static SelectSTBList instance() {
-		if(theInstance == null) 
-		{
+		if (theInstance == null) {
 			theInstance = new SelectSTBList();
 		}
 		return theInstance;
 	}
-	
-	public Vector<STB>getList() {
+
+	public Vector<STB> getList() {
 		return theList;
 	}
 }
