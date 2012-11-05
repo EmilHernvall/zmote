09e3f80c35cb80fcda3e713ce5e8f378abc13608
Fran Valladares <frava356@student.liu.se>
2012-11-05 15:49:36 +0100

    [EPG VIew] Fixed the weird alignment fail

--- src/se/z_app/zmote/gui/EPGFragment.java
+++ src/se/z_app/zmote/gui/EPGFragment.java
@@ -1,6 +1,5 @@
 package se.z_app.zmote.gui;
 
-import javax.xml.datatype.Duration;
 
 import se.z_app.stb.Channel;
 import se.z_app.stb.EPG;
@@ -8,44 +7,38 @@ import se.z_app.stb.Program;
 import se.z_app.stb.api.RemoteControl;
 import se.z_app.zmote.epg.EPGQuery;
 
-import android.app.ActionBar.LayoutParams;
-import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.Matrix;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.HorizontalScrollView;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 
-//This should be a fragment not a Activity
+
+/**
+ * 
+ * @author Thed&Ralf
+ *
+ */
 public class EPGFragment extends Fragment{
 	private String temp;
 	private EPG epg;
 	private View v;
 	private MainTabActivity main;
 	private LinearLayout i_layout;
-
 	private LinearLayout p_layout;
 	private GridLayout g_layout;
-
 	private int counter=0;
 	private HorizontalScrollView hz_scroll;
 	private LinearLayout vt_scroll;
-	
-
 	private int height=80;
 	private int width=80;
 
@@ -58,154 +51,154 @@ public class EPGFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 
-		
-		
 		v = inflater.inflate(R.layout.fragment_epg, null);
-		
 		i_layout = (LinearLayout)v.findViewById(R.id.channel_icons);
-		
 		vt_scroll = (LinearLayout)v.findViewById(R.id.channel_programs);
-		
 		new AsyncDataLoader().execute();
-
 	
 		return v;
 	}
 
-    
-    
-    
-    
-
-
-void mainEPG(){
-	
-	for (Channel channel : epg) {
-		//counter+=1;	
-		//g_layout.setRowCount(counter);
-		addIconToLayout(channel);
-		p_layout = new LinearLayout(v.getContext());
-		/*LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT,
-                LayoutParams.WRAP_CONTENT);
-		p_layout.setLayoutParams(params);*/
-		p_layout.setOrientation(LinearLayout.HORIZONTAL);
-		for (Program program : channel) {
-			addProgramsToLayout(program);	
-		}
+    /**
+     * Fetch the channels
+     */
+	void mainEPG(){
 		
-		vt_scroll.addView(p_layout);
-    	
-     }	
-}
-
-void addIconToLayout(Channel ch){
-	
-	ImageButton new_btn = new ImageButton(v.getContext());
-	new_btn.setId(ch.getNr()+200);	// ID of the button: ChannelNr+200
-	new_btn.setImageBitmap(getResizedBitmap(ch.getIcon(),height,width));
-	new_btn.setBackgroundResource(0);	// Set the background transparent
-	new_btn.setClickable(true);
-	
-	temp = ch.getUrl();
-	
-	
-	
-	//TODO: 
-	//If you click on an icon you are supposed to change channel
-	new_btn.setOnClickListener(new View.OnClickListener() {
-		String url = temp;
-		@Override
-		public void onClick(View v) {
+		for (Channel channel : epg) {
+			//counter+=1;	
+			//g_layout.setRowCount(counter);
+			addIconToLayout(channel);
+			p_layout = new LinearLayout(v.getContext());
+			/*LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT,
+	                LayoutParams.WRAP_CONTENT);
+			p_layout.setLayoutParams(params);*/
+			p_layout.setOrientation(LinearLayout.HORIZONTAL);
+			for (Program program : channel) {
+				addProgramsToLayout(program);	
+			}
 			
-		RemoteControl.instance().launch(url);
-		main.vibrate();
-		}
-		});
-	
-	i_layout.addView(new_btn);
+			vt_scroll.addView(p_layout);
+	    	
+	     }	
+	}
 
-}
-
-int i =0;
-
-//TODO:
-//Make sure it's no space between buttons and they are aligned properly
-void addProgramsToLayout(Program pg){
-	Button new_btn2 = new Button(v.getContext());
-	
-	new_btn2.setText(pg.getName()+" "+pg.getStart());
-	new_btn2.setClickable(true);
-	//new_btn2.setWidth(pg.getDuration()/30);
-	//new_btn2.setHeight(80);
-	
-	
-	LinearLayout.LayoutParams rel_button1 = new LinearLayout.LayoutParams(pg.getDuration()/10, 80);
-	rel_button1.setMargins(0, 0, 0, 0);
-	//rel_button1.height = 80;
-	//rel_button1.width = pg.getDuration()/10;
-	
-	new_btn2.setLayoutParams(rel_button1);
-	
-	
-	new_btn2.setOnClickListener(new View.OnClickListener() {
-	
+	/**
+	 * 
+	 * @param ch
+	 */
+	void addIconToLayout(Channel ch){
 		
-	@Override
-		public void onClick(View v) {
+		ImageButton new_btn = new ImageButton(v.getContext());
+		new_btn.setId(ch.getNr()+200);	// ID of the button: ChannelNr+200
+		new_btn.setImageBitmap(getResizedBitmap(ch.getIcon(),height,width));
+		new_btn.setBackgroundResource(0);	// Set the background transparent
+		new_btn.setClickable(true);
+		temp = ch.getUrl();
 		
-		}
+		//TODO: 
+		//If you click on an icon you are supposed to change channel
+		new_btn.setOnClickListener(new View.OnClickListener() {
+			String url = temp;
+			@Override
+			public void onClick(View v) {
+				
+			RemoteControl.instance().launch(url);
+			main.vibrate();
+			}
 		});
-
-	p_layout.addView(new_btn2, rel_button1);
-
+		
+		i_layout.addView(new_btn);
 	
-}
-
-//Changing the icons size
-public Bitmap getResizedBitmap(Bitmap bm, int newHeight, int newWidth) {
-
-int width = bm.getWidth();
-
-int height = bm.getHeight();
-
-float scaleWidth = ((float) newWidth) / width;
-
-float scaleHeight = ((float) newHeight) / height;
-
-// create a matrix for the manipulation
-
-Matrix matrix = new Matrix();
-
-// resize the bit map
-
-matrix.postScale(scaleWidth, scaleHeight);
-
-// recreate the new Bitmap
-
-Bitmap resizedBitmap = Bitmap.createBitmap(bm, 0, 0, width, height, matrix, false);
-
-return resizedBitmap;
-
-}
-
-
-
-private class AsyncDataLoader extends AsyncTask<Integer, Integer, EPG>{
-
-	@Override
-	protected EPG doInBackground(Integer... params) {
-		EPGQuery query = new EPGQuery();
-		return query.getEPG();
 	}
 	
-	@Override
-	protected void onPostExecute(EPG epgTemp) {
-		epg = epgTemp;
-		v.findViewById(R.id.progressEPGView).setVisibility(View.INVISIBLE);
+	int i =0;
+	
+	//TODO:
+	//Make sure it's no space between buttons and they are aligned properly
+	/**
+	 * Adding programs to the layout
+	 * @param pg
+	 */
+	void addProgramsToLayout(Program pg){
+		Button new_btn2 = new Button(v.getContext());
 		
-		mainEPG();
-	}	
+		new_btn2.setText(pg.getName()+" "+pg.getStart());
+		new_btn2.setSingleLine();
+		new_btn2.setHorizontallyScrolling(true);
+		new_btn2.setEllipsize(TextUtils.TruncateAt.END);
+		new_btn2.setClickable(true);
+		//new_btn2.setWidth(pg.getDuration()/30);
+		//new_btn2.setHeight(80);
+		
+		LinearLayout.LayoutParams rel_button1 = new LinearLayout.LayoutParams(pg.getDuration()/10, 80);
+		rel_button1.setMargins(0, 0, 0, 0);
+		//rel_button1.height = 80;
+		//rel_button1.width = pg.getDuration()/10;
+		
+		new_btn2.setLayoutParams(rel_button1);
+		new_btn2.setOnClickListener(new View.OnClickListener() {
+		
+			
+		@Override
+			public void onClick(View v) {
+			
+			}
+			});
+	
+		p_layout.addView(new_btn2, rel_button1);
+	
+		
+	}
+	
+	/**
+	 * Changes the icons size
+	 * @param bm
+	 * @param newHeight
+	 * @param newWidth
+	 * @return
+	 */
+	public Bitmap getResizedBitmap(Bitmap bm, int newHeight, int newWidth) {
+	
+	int width = bm.getWidth();
+	int height = bm.getHeight();
+	float scaleWidth = ((float) newWidth) / width;
+	float scaleHeight = ((float) newHeight) / height;
+	
+	// Create a matrix for the manipulation
+	Matrix matrix = new Matrix();
+	
+	// Resize the bit map
+	matrix.postScale(scaleWidth, scaleHeight);
+	
+	// Recreate the new Bitmap
+	Bitmap resizedBitmap = Bitmap.createBitmap(bm, 0, 0, width, height, matrix, false);
+	
+	return resizedBitmap;
+	
+	}
 
-}
+
+	/**
+	 * Loads the information asynchronously
+	 * @author 
+	 *
+	 */
+	private class AsyncDataLoader extends AsyncTask<Integer, Integer, EPG>{
+
+		@Override
+		protected EPG doInBackground(Integer... params) {
+			EPGQuery query = new EPGQuery();
+			return query.getEPG();
+		}
+	
+		@Override
+		protected void onPostExecute(EPG epgTemp) {
+			epg = epgTemp;
+			v.findViewById(R.id.progressEPGView).setVisibility(View.INVISIBLE);
+		
+			mainEPG();
+		}	
+
+	}
 
 }
--- src/se/z_app/zmote/gui/WebTVFragment.java
+++ src/se/z_app/zmote/gui/WebTVFragment.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 
 
 public class WebTVFragment extends Fragment {
@@ -16,6 +17,7 @@ public class WebTVFragment extends Fragment {
 	private View view_temp;
 	private MainTabActivity main;
 	private String web_service;  // To know in what service are we currently (youtube, spotify...)
+	private ProgressBar pb;
 
 	
 	
@@ -41,25 +43,52 @@ public class WebTVFragment extends Fragment {
     	return view_temp;
     }    
 	
+	/**
+	 * Calls the back-end function to get the results of a search and shows them
+	 */
 	public void search(){
 		
 		// We can set a progress bar to show the user that we are searching
-		// pb = (ProgressBar)view_temp.findViewById(R.id.progressLodingEpgChannelInformation);
+		 pb = (ProgressBar)view_temp.findViewById(R.id.progressLodingEpgChannelInformation);
 		EditText search_box = (EditText)view_temp.findViewById(R.id.search_box_webtv);
 		String search_for_this = search_box.getText().toString();
 		
 		// Here we should call a function like this
 		// ArrayList<Result> getResultsWebSearch(String searchForThis, String web_service);
+		
+		// After getting the results
+		// pb.setVisibility(View.GONE);	// Quit the progress bar
+		// int type = decideType(web_service);
+		// showResults();
 	}
 	
 	/**
 	 * Print the results of the search on the screen
-	 * @arg res Set of results to show
-	 * @arg type Type of the results: 1-video, 2-Image, 3-Song
+	 * @param res Set of results to show
+	 * @param type Type of the results: 1-video, 2-Image, 3-Song
 	 */
 	/*public void showResults(ArrayList<Result> res, int type){
 		// Types: 1 - video, 2-Image, 3-Song
 		
 		
 	}*/
+	
+	/**
+	 * Decides the type of the result looking at the service we are looking at
+	 * @param web_service	The web service set at the moment
+	 * @return The type of result we should show
+	 */
+	public int decideType(String web_service){
+		int type = 0;
+		if(web_service.compareTo("Youtube") == 0){
+			type = 1;
+		}else if(web_service.compareTo("Spotify") == 0){
+			type = 3;
+		}else if(web_service.compareTo("TED") == 0){
+			type = 1;
+		}
+		
+		return type;
+	}
+	
 }
