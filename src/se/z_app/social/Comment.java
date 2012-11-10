0675023a3de0d87bfe75856876633867c7eccb35
chrte707 <chrte707@student.liu.se>
2012-11-10 12:55:00 +0100

    Added comments to the functions in Comments, Feed and Post. //Harald

--- src/se/z_app/social/Comment.java
+++ src/se/z_app/social/Comment.java
@@ -2,42 +2,95 @@ package se.z_app.social;
 
 import java.util.Date;
 
+/**
+ * Class that describes a comment made to a post
+ * 
+ * @author Rasmus Holm
+ */
 public class Comment {
+	
 	private String userName;
 	private String content;
 	private Date dateOfCreation;
 	private int id;
 	private Post post;
 	
-	public Comment(Post parrentPost){
-		this.post = parrentPost;
+	/**
+	 * Constructor for Comment
+	 * @param parentPost the post the comment is made to
+	 */
+	public Comment(Post parentPost){
+		this.post = parentPost;
 	}
 	
-	public Post getPatentPost(){
+	/**
+	 * Getter for the comment's parent post
+	 * @return the parent post of the comment
+	 */
+	public Post getParentPost(){
 		return post;
 	}
 	
+	/**
+	 * Getter for userName
+	 * @return the user name of the user that made the comment
+	 */
 	public String getUserName() {
 		return userName;
 	}
+	
+	/**
+	 * Setter for userName
+	 * @param userName the name to be set as userName of the comment
+	 */
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+	
+	/**
+	 * Getter for content
+	 * @return the a String with the content of the comment
+	 */
 	public String getContent() {
 		return content;
 	}
+	
+	/**
+	 * Setter for content
+	 * @param content the content to be placed in the comment
+	 */
 	public void setContent(String content) {
 		this.content = content;
 	}
+	
+	/**
+	 * Getter for dateOfCreation
+	 * @return the Date on which the comment was created
+	 */
 	public Date getDateOfCreation() {
 		return dateOfCreation;
 	}
+	
+	/**
+	 * Setter for dateOfCreation
+	 * @param dateOfCreation the Date to be set as the comment's dateOfCreation
+	 */
 	public void setDateOfCreation(Date dateOfCreation) {
 		this.dateOfCreation = dateOfCreation;
 	}
+	
+	/**
+	 * Getter for Id
+	 * @return the ID number of the comment
+	 */
 	public int getId() {
 		return id;
 	}
+	
+	/**
+	 * Setter for Id
+	 * @param id the to-be Id for the comment
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
--- src/se/z_app/social/Feed.java
+++ src/se/z_app/social/Feed.java
@@ -2,49 +2,73 @@ package se.z_app.social;
 
 import java.util.Collection;
 import java.util.Iterator;
-
 import se.z_app.stb.Program;
 import edu.emory.mathcs.backport.java.util.concurrent.ConcurrentSkipListMap;
 
+/**
+ * Class that describes a comment feed that is associated with a program
+ * 
+ * @author Rasmus Holm
+ */
 public class Feed implements Iterable<Post>{
+	
 	private ConcurrentSkipListMap posts = new ConcurrentSkipListMap();
 	private Program program;
 	
+	/**
+	 * Constructor for Feed
+	 * @param program the program that the feed is associated with
+	 */
 	public Feed(Program program){
 		this.program = program;
 	}
 	
+	/**
+	 * Getter for the program that this feed is associated with
+	 * @return the program this feed is associated with
+	 */
 	public Program getProgram() {
 		return program;
 	}
 	
+	/**
+	 * Adds a post to this comment feed
+	 * @param post the post to be added to the feed
+	 */
 	public void addPost(Post post){
 		post.setFeed(this);
 		posts.put(post.getLastUpdate(), post);
 	}
+	
+	/**
+	 * Removes a post from this comment feed
+	 * @param post The post to be removed from the feed
+	 * @return The post that was removed from the feed
+	 */
 	public Post removePost(Post post){
 		return (Post) posts.remove(post.getLastUpdate());
 	}
 	
+	/**
+	 * Get an iterator for all posts made to this feed
+	 * @return an iterator with all posts in this feed
+	 */
 	@SuppressWarnings("unchecked")
 	@Override
 	public Iterator<Post> iterator() {
 		return posts.values().iterator();
 	}
 	
-	
 	/**
 	 * Returns all posts in a feed relating to a specific program.
 	 * @return Post[] ordered by the lastUpdate of the post, from oldest to newest
 	 */
+	@SuppressWarnings("unchecked")
 	public Post[] getPosts(){
-		Collection<Post> com = posts.values();
-		Post[] tbr = new Post[com.size()];
-		com.toArray(tbr);
+		Collection<Post> col = posts.values();
+		Post[] tbr = new Post[col.size()];
+		col.toArray(tbr);
 		return tbr;
 	}
-	
-	
-	
 	
 }
