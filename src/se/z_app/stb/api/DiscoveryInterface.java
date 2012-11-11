4e67d55e430c25a3244dd7dcde7829ebf1411ed0
vikda574 <vikda574@student.liu.se>
2012-11-11 16:29:08 +0100

    Added some comments.

--- src/se/z_app/stb/api/DiscoveryInterface.java
+++ src/se/z_app/stb/api/DiscoveryInterface.java
@@ -2,6 +2,11 @@ package se.z_app.stb.api;
 
 import se.z_app.stb.STB;
 
+/**
+ * The interface for the Discovery mechanism in a STB box
+ * @author Rasmus Holm
+ *
+ */
 public interface DiscoveryInterface {
 	public STB[] find();
 	public STB[] find(STB stb);
