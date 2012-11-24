33edb39181ea735c021d761215189d3da6ca9557
linba708 <linba708@student.liu.se>
2012-11-24 16:50:13 +0100

    Social TV now shows posts.

--- src/se/z_app/zmote/gui/ZChatActivity.java
+++ src/se/z_app/zmote/gui/ZChatActivity.java
@@ -1,16 +1,22 @@
 package se.z_app.zmote.gui;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import se.z_app.social.Feed;
 import se.z_app.social.PostInterface;
 import se.z_app.social.zchat.ZChatAdapter;
 import se.z_app.social.Post;
 import se.z_app.stb.Program;
+import se.z_app.stb.STB;
+import se.z_app.stb.api.EPGData;
+import se.z_app.zmote.epg.EPGContentHandler;
 import android.app.Activity;
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -28,11 +34,14 @@ public class ZChatActivity extends Activity {
 
 	 public void onCreate(Bundle savedInstanceState){
 		 super.onCreate(savedInstanceState);
-		 setContentView(R.layout.zchat_list);
+		 setContentView(R.layout.activity_zchat);
 		 TextView textView = (TextView) findViewById(R.id.feed_name);
 		 textView.setText(getIntent().getExtras().getString("program"));
-		 
-		 new AsyncDataLoader();
+		 textView = (TextView) findViewById(R.id.time_of_feedUpdate);
+		 textView.setText("50 min ago");
+		 ListView postList = (ListView) findViewById(R.id.list_over_post);
+
+		 new AsyncDataLoader(this, postList).execute();
 		 
 	 }
 	 
@@ -40,24 +49,25 @@ public class ZChatActivity extends Activity {
 	
 	private class FragmentAdabter extends BaseAdapter{
 
-		private Fragment fragment;
+		private ZChatActivity zChatActivity;
 		private Feed feed;
-		private ArrayList<PostInterface> list;
+
+		private ArrayList<Post> list;
+
 		
-		public FragmentAdabter(Fragment fragment, Feed feed){
-			this.fragment = fragment;
+		public FragmentAdabter(ZChatActivity zChatActivity , Feed feed){
+			this.zChatActivity = zChatActivity;
 			this.feed = feed;
-			list = new ArrayList<PostInterface>();
-			populateArrayList();
-			
-			
-		}
-		private void populateArrayList() {
-			for(Post post : feed){
-				list.add(0, post);
-				list.addAll(1, post.getCommentsAsCollection());
+			Iterator<Post> iter = feed.iterator();
+			list = new ArrayList<Post>();
+			while(iter.hasNext()){
+				list.add(0, iter.next());
+
 			}
+			
+			
 		}
+
 		@Override
 		public int getCount() {
 			
@@ -65,7 +75,7 @@ public class ZChatActivity extends Activity {
 		}
 
 		@Override
-		public Object getItem(int position) {
+		public Post getItem(int position) {
 			return list.get(position);
 		}
 
@@ -76,18 +86,53 @@ public class ZChatActivity extends Activity {
 
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
-			// TODO Auto-generated method stub
-			return null;
+			System.out.println("The number of comments are: "+list.get(position).getCommentsAsCollection().size());
+
+	        View vi=convertView;
+	        if(convertView==null){
+	            vi = ((LayoutInflater)zChatActivity.getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.zchat_list, null);
+	        }
+	        
+	        TextView userName = (TextView) vi.findViewById(R.id.post_user_name);
+	        TextView date = (TextView) vi.findViewById(R.id.post_date);
+	        TextView content = (TextView) vi.findViewById(R.id.post_content);
+	        TextView nrOfComments = (TextView) vi.findViewById(R.id.post_nr_of_comments);
+	        
+	        
+	        userName.setText(list.get(position).getUserName());
+	        date.setText(list.get(position).getDateOfCreation().toString());
+	        content.setText(list.get(position).getContent());
+	        nrOfComments.setText(list.get(position).getComments().length + " comments");
+	        
+	        
+	        
+	        return vi;
 		}
+		
+		
 		
 	}
 	
-	private class AsyncDataLoader extends AsyncTask<Object, Object, Object>{
+	private class AsyncDataLoader extends AsyncTask<Integer, Integer, Feed>{
 
+		private ListView postList;
+		private ZChatActivity activity;
+		public AsyncDataLoader(ZChatActivity activity ,ListView postList){
+			this.postList = postList;
+			this.activity = activity;
+		}
 		@Override
-		protected Object doInBackground(Object... arg0) {
+		protected Feed doInBackground(Integer... arg0) {
+			ZChatAdapter adapter = new ZChatAdapter();
+			System.out.println(EPGContentHandler.instance().getEPG().iterator().next().iterator().next().getName());
+			return adapter.getFeed(EPGContentHandler.instance().getEPG().iterator().next().iterator().next());
+			
+
+		}
 		
-			return null;
+		protected void onPostExecute(Feed feed){
+			 postList.setAdapter(new FragmentAdabter(activity, feed));
+			
 		}
 		
 	}
