7ea0e2028a4ff300dfc44f4b395acdda645e2cc8
harbr840 <harbr840@student.liu.se>
2012-11-25 17:35:10 +0100

    Added and improved javadoc comments for STBListener

--- src/se/z_app/stb/api/STBListener.java
+++ src/se/z_app/stb/api/STBListener.java
@@ -8,33 +8,44 @@ import se.z_app.stb.STB;
 import se.z_app.stb.STBEvent;
 
 /**
- * Class that listens to a eventlistener.
+ * Class that listens to a event listener.
+ * 
  * @author Linus Back
- *
  */
 public class STBListener extends Observable implements Observer, Runnable{
 
-	//Singleton and adding itself as an observer
 	private EventListnerInterface eventListener;
 
 	private Thread myThread;
 	private STB stb;
 	private STBEvent event;
 	
+	/**
+	 * Creates and holds the singleton instance of the STB listener
+	 * @author Linus Back
+	 */
 	private static class SingletonHolder { 
         public static final STBListener INSTANCE = new STBListener();
 	}
 		
+	/**
+	 * Request the singleton instance of the STB listener
+	 * @return The instance of STB listener
+	 */
 	public static STBListener instance(){
 		return SingletonHolder.INSTANCE;
 	}
+	
+	/**
+	 * Private constructor for the STB listener
+	 */
 	private STBListener(){
 		STBContainer.instance().addObserver(this);
-
 	}
 	
 	/**
-	 * Might be unnecessary with this override, needs to look at this more
+	 * Adds an observer object to the STB listener instance
+	 * @param observer - The observer to be added
 	 */
 	@Override
 	public void addObserver(Observer observer) {
@@ -42,17 +53,20 @@ public class STBListener extends Observable implements Observer, Runnable{
 		if(eventListener !=null && eventListener.getCurrentEvent() != null){
 			this.setChanged();
 			this.notifyObservers(eventListener.getCurrentEvent());
-		}
-		
+		}	
 	}
 	
-	
-
+	/**
+	 * Getter for the current STB event
+	 * @return The current STB event
+	 */
 	public STBEvent getCurrentEvent(){
 		return event;
 	}
 	
-	
+	/**
+	 * Updates the STB listener
+	 */
 	public void update(Observable obsesrvable, Object data) {
 		stb = STBContainer.instance().getActiveSTB();
 		if(eventListener != null)
@@ -64,6 +78,9 @@ public class STBListener extends Observable implements Observer, Runnable{
 		
 	}
 
+	/**
+	 * Initiates the STB listener
+	 */
 	public void run() {
 		
 		System.out.println("Initiating Listner");
