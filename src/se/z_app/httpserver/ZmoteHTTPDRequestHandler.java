6fdebdd5180ba9584e63b218b14d6542cc7cfc9b
harbr840 <harbr840@student.liu.se>
2012-11-25 14:36:56 +0100

    Added javadoc comments to ZmoteHTTPDRequestHandler

--- src/se/z_app/httpserver/ZmoteHTTPDRequestHandler.java
+++ src/se/z_app/httpserver/ZmoteHTTPDRequestHandler.java
@@ -4,6 +4,11 @@ import java.util.Properties;
 
 import se.z_app.httpserver.NanoHTTPD.Response;
 
+/**
+ * Interface for handlers of HTTPD requests
+ * 
+ * @author Rasmus Holm
+ */
 public interface ZmoteHTTPDRequestHandler{
 	public String getURI();
 	public Response serve(String uri, String method, Properties header, Properties parms, Properties files, ZmoteHTTPD httpd);	
