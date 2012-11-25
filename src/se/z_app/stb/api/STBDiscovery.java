1a9e4f620b8ffe6080217101ae1f51f0e5de8be2
harbr840 <harbr840@student.liu.se>
2012-11-25 17:30:35 +0100

    Improved javadoc comments in STBDiscovery

--- src/se/z_app/stb/api/STBDiscovery.java
+++ src/se/z_app/stb/api/STBDiscovery.java
@@ -4,22 +4,25 @@ import se.z_app.stb.STB;
 import se.z_app.stb.api.zenterio.Discovery;
 
 /**
- *
- * Calls the Discovery.find() for finding the customers box.
+ * Class that searches for STBs on the current network
+ * 
  * @author Viktor Dahl
- *
  */
 public class STBDiscovery {
 	String subNetAddress;
 	Discovery disc;
 	
+	/**
+	 * Creates a new STBDiscovery with the specified sub-net address
+	 * @param subNetAddress - The sub-net address to use
+	 */
 	public STBDiscovery(String subNetAddress) {
 		this.subNetAddress = subNetAddress;
 	}
 	
 	/**
-	 * 
-	 * @return An array STB[] with the STB's found.
+	 * Search for all STBs on the current network
+	 * @return An array with all STB's that were found.
 	 */
 	public STB[] find() {
 		disc = new Discovery(subNetAddress);
