ed9249d1f30eea978044ce582e6eaf3b4063da06
harbr840 <harbr840@student.liu.se>
2012-11-25 17:47:34 +0100

    Added and improved javadoc comments for RCProxy

--- src/se/z_app/stb/api/RCProxy.java
+++ src/se/z_app/stb/api/RCProxy.java
@@ -6,23 +6,35 @@ import java.util.Observer;
 import se.z_app.stb.STBEvent;
 
 /**
- * needs a descirption here...
+ * Class responsible for forwarding button presses to the RCProxyState implementation in use
+ * 
  * @author Leonard Jansson, Viktor von Zeipel, refractored by Rasmus Holm, Linus Back
- * TODO: Implementation.
  */
 public class RCProxy implements Observer{
 
 
 	private RCProxyState state;
 	
-	//Singleton
+	/**
+	 * Creates and holds the singleton instance of the RC proxy
+	 * @author Rasmus Holm, Linus Back
+	 */
 	private static class SingletonHolder { 
         public static final RCProxy INSTANCE = new RCProxy();
 	}
+	
+	/**
+	 * Request the singleton instance of the RC proxy
+	 * @return The instance of the RC proxy
+	 */
 	public static RCProxy instance(){
 		return SingletonHolder.INSTANCE;
 	}
 	
+	/**
+	 * Private constructor that creates the singleton instance as a DefaultState and 
+	 * adds it as a listener to STBListener
+	 */
 	private RCProxy() {
 		state = new DefaultState();
 		STBListener.instance().addObserver(this);
