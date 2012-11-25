cd723f1197ecaa6a6a202d78838d708418049455
harbr840 <harbr840@student.liu.se>
2012-11-25 12:06:25 +0100

    Added javadoc comments to Bootstrap

--- src/se/z_app/zmote/gui/Bootstrap.java
+++ src/se/z_app/zmote/gui/Bootstrap.java
@@ -11,13 +11,27 @@ import se.z_app.stb.api.WebTVCommand;
 import se.z_app.zmote.epg.EPGContentHandler;
 import se.z_app.zmote.webtv.MediaStreamer;
 
+/**
+ * Instantiates necessary components and saves data like the phone ip address
+ * 
+ * @author Rasmus Holm
+ */
 public class Bootstrap {
 	private static String ip;
 	
+	/**
+	 * Getter for the smartphone device's local ip address
+	 * @return the ip address
+	 */
 	public static String getLocalIP(){
 		return ip;
 	}
 	
+	/**
+	 * Constructor for Bootstrap
+	 * @param context the application context
+	 * @param wifiManager a WifiManager for the application
+	 */
 	public Bootstrap(Context context, WifiManager wifiManager){
 		
 		EPGContentHandler.setContext(context);
